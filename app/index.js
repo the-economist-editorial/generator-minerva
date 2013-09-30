5e0c5fbc447d639cd30c1481a71c78c2bc5b48e2
angelorepole <angelorepole@economist.com>
2013-09-30 13:47:12 +0100

    created the generator-minerva v0.0.1

---
+++ app/index.js
@@ -0,0 +1,54 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var EdiGenerator = module.exports = function EdiGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(EdiGenerator, yeoman.generators.Base);
+
+EdiGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    name: 'projectName',
+    message: 'What\'s the name of the project?'
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.projectName = props.projectName;
+
+    cb();
+  }.bind(this));
+};
+
+EdiGenerator.prototype.app = function app() {
+  this.mkdir('css');
+  this.mkdir('css/sass');
+  this.mkdir('js');
+  this.mkdir('js/tpl');
+  this.mkdir('js/tpl/handlebars');
+  this.mkdir('js/tests');
+  this.copy('css/style.css', 'css/style.css');
+  this.copy('css/sass/style.scss', 'css/sass/style.scss');
+  this.copy('js/init.js', 'js/init.js');
+  this.copy('js/tpl/template.js', 'js/tpl/template.js');
+  this.copy('js/tpl/handlebars/tpl.handlebars', 'js/tpl/handlebars/tpl.handlebars');
+  this.copy('js/tests/tests.js', 'js/tests/tests.js');
+  this.copy('index.html', 'index.html');
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+  this.copy('Gruntfile.js', 'Gruntfile.js');
+};
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,119 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.{% if (package_json) { %}
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: ['js/**/*.js', '!js/init.min.js', '!js/tests/*.js'],
+        dest: 'js/concat.js'
+      }
+    },
+    uglify: {
+      dist: {
+        src: 'js/concat.js',
+        dest: 'js/init.min.js'
+      }
+    },
+   watch: {
+      sass: {
+        files: ['css/sass/**/*.{scss,sass}',  '../../css/editorial.scss'],
+        tasks: ['sass:dev']
+      },
+      handlebars: {
+        files: ['js/tpl/handlebars/*.handlebars'],
+        tasks: ['handlebars']
+      }
+    },
+    sass: {
+      dist: {
+        options: {                      // Dictionary of render options
+          outputStyle: [
+            'compressed'
+          ]
+        },
+        files: {
+          'css/style.css': 'css/sass/style.scss'
+        }
+      },
+      dev: {
+        files: {
+          'css/style.css': 'css/sass/style.scss'
+        }
+      }
+    },
+    jshint: {
+      files: ['js/**/*.js', '!js/init.min.js',  '!js/tpl/template.js'],
+      options: {
+        camelcase: true,
+        curly:   true,
+        eqeqeq:  true,
+        forin: true,
+        immed:   true,
+        latedef: true,
+        newcap:  true,
+        noarg:   true,
+        sub:     true,
+        undef:   true,
+        boss:    true,
+        eqnull:  true,
+        browser: true,
+        strict: true,
+        trailing: true,
+
+        globals: {
+          // AMD
+          module:     true,
+          require:    true,
+          requirejs:  true,
+          define:     true,
+          Handlebars: true,
+
+          // Environments
+          console:    true,
+
+          // General Purpose Libraries
+          $:          true,
+          jQuery:     true,
+          sinon:      true,
+          describe:   true,
+          it:         true,
+          expect:     true,
+          beforeEach: true,
+          afterEach:  true
+        }
+      }
+    },
+    handlebars: {
+      all: {
+        files: {
+            'js/tpl/template.js': 'js/tpl/handlebars/*.handlebars'
+        }
+      }
+    },
+    jasmine: {
+      src: ['js/*.js', '../../js/*.js', '!js/init.min.js'],
+      options: {
+        specs: ['js/tests/*tests.js', '../../js/tests/*tests.js'],
+      }
+    },
+   clean: {
+      clean: ["js/concat.js"]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-handlebars-compiler');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('production', ['concat', 'uglify', 'sass:dist', 'clean']);
+  grunt.registerTask('compile', ['sass:dev', 'handlebars', 'watch']);
+  // Unit Testing Task
+  grunt.registerTask('test', ['jasmine', 'jshint']);
+};
