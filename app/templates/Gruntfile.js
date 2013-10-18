9d49ac4beac9f96f05bc051cdafab1b1337a6509
angelorepole <angelorepole@economist.com>
2013-10-18 15:12:27 +0100

    Removed normalize css replaced with reset from eco.com cdn same for query

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
     },
     sass: {
       dist: {
-        options: {                      // Dictionary of render options
+        options: {
           outputStyle: [
             'compressed'
           ]
