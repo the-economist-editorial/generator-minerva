c0678c1288dc8f7bc05eb6c8d538068007fa8a1a
buddhamagnet <buddhamagnet@gmail.com>
2013-10-19 18:05:36 +0100

    run githooks command when dependencies have been installed

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ var EdiGenerator = module.exports = function EdiGenerator(args, options, config)
   });
 
   this.on('dependenciesInstalled', function() {
-    this.spawnCommand('grunt', ['shell']);
+    this.spawnCommand('grunt', ['shell', 'githooks']);
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
